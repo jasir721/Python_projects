40e503fcaafc14814cd90cc8bab8ab2fc0564a41
Jasir K <62549297+jasir721@users.noreply.github.com>
2021-05-21 00:22:45 +0530

    Add files via upload

---
+++ Rain_alert_SMS_API/main.py
@@ -0,0 +1,42 @@
+#API endpoints and API parameters we have checked
+#Previously what ever we used were from free API's
+#Many will cange bcause it is difficult to collect those data's
+#For free service there is a API key
+
+import requests
+from twilio.rest import Client
+End_point="https://api.openweathermap.org/data/2.5/onecall"
+api_key="5169fd925017438b782fb55fccbc3563"
+LAT="28.066700"
+LON="95.327500"
+account_sid = "ACfe1520255d2898e4951a9a528e49ea49"
+auth_token ="520a7490e80c4ad44411e53dbe68256e"
+weather_params = {
+    "lat":LAT,
+    "lon":LON,
+    "exclude":"current,minutely,daily",
+    "appid":api_key,
+}
+response=requests.get(End_point,params=weather_params)
+response.raise_for_status()
+weather_data=response.json()["hourly"]
+# print(weather_data)
+will_rain=False
+for i in range(12):
+    hour=weather_data[i]
+    weather=hour["weather"][0]
+    print(weather)
+    weather_id=weather["id"]
+    if weather_id <600:
+        will_rain= True
+        print("rain")
+if will_rain:
+    client = Client(account_sid, auth_token)
+    message = client.messages \
+        .create(
+        body="It's going to rain today. Remember to take umbrella",
+        from_='+18036536998',
+        to='+918802607303'
+    )
+    print(message.status)
+
